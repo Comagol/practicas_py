4beda1432deb6a01164aa99152c4be53775173a0
Fran Comabella <comabellafrancisco@gmail.com>
2025-01-06 23:23:18 -0300

    Primer commit de las practicas de Python

---
+++ 01_operadores_y_estructuras_de_control.py
@@ -0,0 +1,53 @@
+"""
+OPERADORES
+"""
+
+# Operadores Aritméticos
+print(f"Suma: 10 + 3 = {10+3}")
+print(f"Resta: 10 - 3 = {10-3}")
+print(f"Multiplicacion: 10 * 3 = {10*3}")
+print(f"division: 10 / 3 = {10/3}")
+print(f"Modulo: 10 % 3 = {10%3}")
+print(f"Exponente: 10 ** 3 = {10**3}")
+print(f"Division entera: 10 // 3 = {10//3}")
+
+# Operadores de Comparacion
+print(f"Igualdad: 10 == 3 es {10 == 3}")
+print(f"Desigualdad: 10 != 3 es {10!=3}")
+print(f"Mayor que: 10 > 3 es {10>3}")
+print(f"Menor que: 10 < 3 es {10<3}")
+print(f"Mayor o igual que: 10 >= 3 es {10>=3}")
+print(f"Menor o igual que: 10 >= 3 es {10<=3}")
+
+# Operadores Logicos
+print(f"AND &&: 10 +3 == 13 and 5 - 1 == 4 es {10+3==13 and 5-1 == 4}")
+print(f"OR ||: 10 +3 == 14 or 5 - 1 == 4 es {10+3==14 or 5-1 == 4}")
+print(f"NOT !: not 10 + 3 == 14 es {not 10 + 3 == 14}")
+
+# Operadores de Asignacion
+my_number = 11 #asignacion 
+print(my_number)
+my_number += 1 #suma y asignacion
+print(my_number)
+my_number -= 1 #resta y asignacion
+print(my_number)
+my_number *= 2 #multiplicacion y asignacion
+print(my_number)
+my_number /= 2 #division y asignacion
+print(my_number)
+my_number %= 3 #modulo y asignacion
+print(my_number)
+my_number **= 2 #exponente y asignacion
+print(my_number)
+my_number //= 2 #division entera y asignacion
+print(my_number)
+
+# Operadores de Identidad
+my_new_number = my_number
+print(f"my_number is my_new_number es {my_number is my_new_number}")
+print(f"my_number is my_new_number es {my_number is not my_new_number}")
+
+#Operadores de Pertenencia
+print(f"'f' in 'francisco' = {'f' in 'francisco'}")
+print(f"'h' in 'francisco' = {'h' not in 'francisco'}")
+
