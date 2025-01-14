89d74897055c1269443fb27a95ca0c92ef5d1768
Comagol <comabellafrancisco@gmail.com>
2025-01-14 19:21:41 -0300

    Ejercicios practicos con Stack y Queue en Python.

--- 07_practica.py
+++ 07_practica.py
@@ -25,3 +25,51 @@ queue.append("3")
 queue.pop(0)
 print(queue)
 
+"""
+Extra
+"""
+
+#Web
+
+def web_navigation():
+    stack = []
+    
+    while True :
+        action = input(
+            "añade un URL o interactua con las palabras adelante/atras/salir: "
+        )
+        
+        if action == "salir":
+            print("Saliendo del navegador web.")
+            break
+        elif action == "adelante":
+            pass
+        elif action == "atras":
+            if len(stack) >0:
+                stack.pop()
+        else:
+            stack.append(action)
+        if len(stack) > 0:
+            print(f"Has navegado a la web: {stack[len(stack)-1]}.")
+        else:
+            print("Estas en la pagina principal")
+
+#web_navigation()
+
+
+def shared_printed():
+    queue = []
+    while True :
+        
+        action = input("Añade un documento a la cola o selecciona imprimir / salir: ")
+        if action == "salir":
+            break
+        elif action == "imprimir":
+            if len(queue) > 0:
+                print(f"Imprimiendo: {queue.pop(0)}")
+        else:
+            queue.append(action)
+        
+        print(f"Cola de impresion: {queue}")
+    
+shared_printed()
